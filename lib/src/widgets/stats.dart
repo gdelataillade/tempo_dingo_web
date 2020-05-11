f5d89210e33f31dbf5c1dd77b6775816ff82356d
Gautier <gautier2406@gmail.com>
2020-05-11 15:51:27 +0200

    Delete useless tabs widget

--- lib/src/widgets/stats.dart
+++ lib/src/widgets/stats.dart
@@ -7,19 +7,12 @@ class Stats extends StatefulWidget {
 }
 
 class _StatsState extends State<Stats> {
-  Future<int> _getGamesNb() async {
-    final QuerySnapshot snapshot =
-        await Firestore.instance.collection("stats").getDocuments();
-    return snapshot.documents.first["nbGames"];
-  }
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: Firestore.instance.collection('stats').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return Text("No data");
-        print(snapshot.data.documents.first["nbGames"]);
         return Center(
           child: Text(
             "Games played: ${snapshot.data.documents.first["nbGames"]}",
